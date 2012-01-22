df4ba06660cd8e8b49eac256bcd753b0f706589a
Eugene Chuvyrov <echuvyrov@msn.com>
2012-01-22 18:01:09 -0500

    Initial check-in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrackingETFs::Application.config.secret_token = 'edeef270cb83f1c001d6baf5a34ab2f28bdbe45d8feffdc04988d4cd0e9a4835cc84683e54a587e095d5d0250384b649e0ae137e288f5004ad48c7d7c9ab33e2'
